4c96997fbdb6e1a6a07518666a19a7ba0c9c038c
dking1342 <leanmachine42@yahoo.com>
2020-11-20 16:27:36 -0600

    added config and init to server.js file

--- server.js
+++ server.js
@@ -1,20 +1,66 @@
-// server.js
-// where your node app starts
 
-// we've started you off with Express (https://expressjs.com/)
-// but feel free to use whatever libraries or frameworks you'd like through `package.json`.
+// npm packages
 const express = require("express");
+const path = require('path');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const errorhandler = require('errorhandler');
+
+// config mongoose's promise to global promise
+mongoose.promise = global.Promise;
+
+// config isproduction variable
+const isProduction = process.env.NODE_ENV === 'production';
+
+// init app
 const app = express();
-const port = parseInt(process.env.PORT) || 3000;
+
+// config the app
+app.use(cors());
+app.use(require('morgan')('dev'));
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+app.use(express.static("public"));
+app.use(session({secret:'passport-tutorial',cookie:{maxAge:60000},resave:false,saveUninitialized:false}));
+
+
+if(!isProduction){
+  app.use(errorhandler());
+}
+
+// config mongoose
+mongoose.connect('mongodb://localhost/passport-tutorial',{useUnifiedTopology:true,useNewUrlParser:true});
+mongoose.set('debug',true);
+
+
+// error handler and middleware
+if(!isProduction){
+  app.use((err,req,res)=>{
+    res.status(err.status || 500);
+
+    res.json({
+      errors:{
+        message:err.message,
+        error:err,
+      }
+    })
+  })
+}
+
+app.use((err,req,res)=>{
+  res.status(err.status || 500);
+
+  res.json({
+    errors:{
+      message: err.message,
+      error:{},
+    }
+  })
+})
+
 
 // listen for requests :)
+const port = parseInt(process.env.PORT) || 3000;
 app.listen(port,()=> console.log(`Listening on port ${port}`));
-
-// make all the files in 'public' available
-app.use(express.static("public"));
-
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/views/index.html");
-});
-
-
